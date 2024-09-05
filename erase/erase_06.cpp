2fffcf21d27fc7106b9bc3e215299be23bb79665
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-05 18:14:57 +0300

    Create erase_06.cpp

---
+++ erase/erase_06.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string s{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+
+	std::cout << '|' << s << "|\n";
+	s.clear(); // erases all chars
+
+	std::cout << '|' << s << "|\n";
+}
