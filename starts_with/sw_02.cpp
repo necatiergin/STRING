f930f62f8794109d2b5eb5b67b0571fdf93c0040
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-09 18:37:37 +0300

    Create sw_02.cpp

---
+++ starts_with/sw_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+//starts_with since C++17
+
+int main()
+{
+	std::cout << std::boolalpha;
+	std::string name{ "necatiergin" };
+
+	std::cout << name.starts_with("necati") << '\n';
+	std::cout << name.starts_with("nedim") << '\n';
+}
