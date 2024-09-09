5e05aac0f48417e06d4af8603df0f4388cf5fa21
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-09 18:38:29 +0300

    Update sw_01.cpp

--- starts_with/sw_01.cpp
+++ starts_with/sw_01.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 
+//starts_with since C++20
 int main()
 {
 	std::cout << std::boolalpha;
 	std::string name{ "necatiergin" };
 
-	std::cout << name.starts_with('n') << '\n';
-	std::cout << name.starts_with('k') << '\n';
+	std::cout << name.starts_with("necati") << '\n';
+	std::cout << name.starts_with("nedim") << '\n';
 }
