a61707a1aabf7686e7d4a7a1ef62f4e7aac4d769
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-11 17:38:41 +0300

    Update erase_13.cpp

--- erase/erase_13.cpp
+++ erase/erase_13.cpp
@@ -1,11 +1,12 @@
-Build started at 17:37...
-1>------ Build started: Project: 1TEMMUZ2024CPP, Configuration: Release Win32 ------
-1>main.cpp
-1>Generating code
-1>33 of 84 functions (39.3%) were compiled, the rest were copied from previous compilation.
-1>  3 functions were new in current compilation
-1>  4 functions had inline decision re-evaluated but remain unchanged
-1>Finished generating code
-1>1TEMMUZ2024CPP.vcxproj -> C:\Users\necat\source\repos\1TEMMUZ2024CPP\Release\1TEMMUZ2024CPP.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 17:38 and took 02,065 seconds ==========
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string name{ "necatiergin" };
+
+	while (!name.empty()) {
+		std::cout << name << '\n';
+		name.pop_back();
+	}
+}
