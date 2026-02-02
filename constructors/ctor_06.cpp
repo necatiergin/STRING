f465e5ec99f84f2002851cd35a98ebcb9108a06f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-02 14:50:55 +0300

    Update ctor_06.cpp

--- constructors/ctor_06.cpp
+++ constructors/ctor_06.cpp
@@ -4,9 +4,7 @@
 
 int main()
 {
-	using namespace std;
-
-	vector<char>cvec{ 66, 65, 66, 65 };
-	string str{ cvec.begin(), cvec.end() }; //range ctor
+	std::vector<char>cvec{ 66, 65, 66, 65 };
+	std::string str{ cvec.begin(), cvec.end() }; //range ctor
 	std::cout << str << '\n';
 }
